90b1cdda4fe6b71adfecdee210b242e477ff9445
zzaxe1zz <axelgomezfra09@gmail.com>
2026-06-01 08:58:03 -0600

    Agregue la practia 1 al repositorio

---
+++ p1-strings.py
@@ -0,0 +1,18 @@
+# 1. Escribir un programa que muestre por pantalla la cadena ¡Hola mundo!
+print("¡Hola mundo!")
+
+# 2. Escribir un programa que guarde en variable la cadena ¡Estoy aprendiendo Python!
+# Y que luego muestre en pantalla el valor de esa variable.
+string = "¡Estoy aprendiendo Python!"
+print(string)
+# 3. Escriba un programa que le pregunte al usuario su nombre para luego saludarlo por consola por su nombre.
+nombre = input('Ingrese su nombre: ')
+print('Bienvenido,', nombre)
+# 4. Defina tres variables: una que reciba un valor flotante, un valor entero y un string.
+entradaFloat = float(input('Ingrese un decimal: '))
+entradaInt = int(input('Ingrese un entero: '))
+entradaStr = input('Ingrese un texto: ')
+
+print(entradaFloat)
+print(entradaInt)
+print(entradaStr)
