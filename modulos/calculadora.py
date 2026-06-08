a1a091bd41a0fa16da1e10c6df151e7f0e51524b
zzaxe1zz <axelgomezfra09@gmail.com>
2026-06-08 07:57:53 -0600

    modulos

---
+++ modulos/calculadora.py
@@ -0,0 +1,6 @@
+from modulos import suma
+from modulos import *
+
+suma(3, 3)
+resta(10, 5)
+dividir(10, 2)
---
+++ modulos/modulos.py
@@ -0,0 +1,10 @@
+def suma(op1, op2):
+    print(f'El resultado de la suma es: {op1+op2}')
+
+
+def resta(op1, op2):
+    print(f'El resultado de la resta es: {op1-op2}')
+
+
+def dividir(op1, op2):
+    print(f'El resultado de la division es: {op1/op2}')
